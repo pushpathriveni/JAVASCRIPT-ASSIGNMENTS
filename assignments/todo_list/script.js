311b11998d0d36ad48e5f373579065ea12eb23cc
THRIVENI PERUR <tperur@gmail.com>
2022-02-08 16:27:32 +0530

    ASSIGNMENT-8

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,17 +1,69 @@
-var data = document.querySelector("input[name='todo_input']");
-var submit = document.querySelector('button');
-var filldata = document.querySelector('.todos');
-var count = 0;
-submit.addEventListener('click', function () {
-    var tag = document.createElement('p');
-    var text = data.value;
-    tag.append(text);
-    tag.setAttribute('key', count);
-    filldata.append(tag);
-    count += 1;
-})
-filldata.addEventListener("click", removebtn);
-function removebtn(e) {
-    console.log(e.target.innerText);
-    filldata.removeChild(e.target);
-}
+<html>
+     <head>
+         <title>Todo List</title>
+     </head>
+     <body>
+         <div>
+             <input type='text' name='todo_input'>
+             <button type='submit'>Show Pending / Completed</button>
+             <select id='dropdown'>
+                 <option value="list">List</option>
+                 <option id="completed" value="completed">Completed</option>
+                 <option id="pending" value="pending">Pending</option>
+             </select>
+             <div class='todos'></div>
+         </div>
+     </body>
+     <script src='./script.js'></script>
+ </html>
+let todos = [];
+ var addToDoButton = document.querySelector('button');
+ var todo = document.querySelector('.todos')
+ let drop = document.querySelector("#dropdown");
+ // var i = 0;
+ // var inputfield = document.querySelector('input');
+ addToDoButton.addEventListener("click", work);
+ async function main() {
+     try {
+         const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+         const json = await response.json();
+         todos = json.slice(0, 10);
+         console.log(todos);
+         todos.forEach((todom,index)=>{
+             const newTodo = document.createElement('p');
+             newTodo.setAttribute('key', index);
+             newTodo.innerHTML = todom.title
+             todo.appendChild(newTodo);
+         })
+     }catch(e) {
+         console.log(e);
+     }
+ }
+ function work(){
+     if(drop.value ==="list"){
+         main()
+     }else if(drop.value === "completed"){
+         completed()
+     }else if(drop.value === "pending"){
+         pending()
+     }
+ }
+ function completed(){
+     todo.innerHTML = ""
+     todos.filter(todom => todom.completed).forEach((todom,index)=>{
+         const newTodo = document.createElement('p')
+         newTodo.setAttribute('key',index)
+         newTodo.innerHTML = todom.title
+         todo.appendChild(newTodo);
+     })
+ }
+ function pending(){
+     todo.innerHTML = ""
+     todos.filter(todom => !todom.completed).forEach((todom,index)=>{
+         const newTodo = document.createElement('p')
+         newTodo.setAttribute('key',index)
+         newTodo.innerHTML = todom.title
+         todo.appendChild(newTodo);
+     })
+ }
+ main()
